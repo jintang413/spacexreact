1e6a669e75482f171b4afe91fce554c96d9e9bb1
Jin Tang <jintang413@gmail.com>
2021-07-07 23:30:45 -0400

    more changes

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(
   }),
 );
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
 
